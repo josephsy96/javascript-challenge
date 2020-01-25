e9146417c6caa1481656b0ffe1b48f45d1ce9736
josephsy96 <josephsy96@hotmail.com>
2020-01-24 20:18:48 -0600

    some javascript

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
 
+var filter_button = d3.select("#button");
 
+filter_button.on("click", function() {
+
+    var input_filter = d3.select("#datetime");
+
+
+
+}
+
+);
